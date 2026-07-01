4bb251516c040c27de0fee9306672a484dc3eac4
Johny-JetLearner <giannistempelis00@gmail.com>
2026-07-01 10:52:51 +0300

    Add files via upload

--- letter.py
+++ letter.py
@@ -1,12 +1,16 @@
 import pygame
+pygame.init()
 import time
 screen=pygame.display.set_mode((1000,900))
 image_1=pygame.image.load(r"C:\Users\pc\Desktop\SiderAddons\Python\pro game developer\images (1).jpeg")
 image_2=pygame.image.load(r"C:\Users\pc\Desktop\SiderAddons\Python\pro game developer\images (2).jpeg")
 image_44=pygame.transform.scale(image_1,(1000,900))
 image_33=pygame.transform.scale(image_2,(1000,900))
+font=pygame.font.SysFont("Serif",65)
+text=font.render("Thanks for everything Mum and Dad",True,"white")
 while True:
     screen.blit(image_44,(0,0))
+    screen.blit(text,(5,100))
     pygame.display.update()
     time.sleep(2)
     screen.blit(image_33,(0,0))
